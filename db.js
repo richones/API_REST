20adf9088dcfc9a63a6bccd7a5874ccb75a328df
emngaiden <emngaiden@hotmail.com>
2018-09-12 19:36:03 -0500

    Update db.js
    Agregado String de conexión

--- db.js
+++ db.js
@@ -1,2 +1,2 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://yourMongoDBURIGoesHere');
+mongoose.connect('mongodb://testuser:testuserpassword123@ds028559.mlab.com:28559/foodseek');
